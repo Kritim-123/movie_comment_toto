a9f0d9ac2ecd75a42b3a110bda83224dce85b9dc
Kritim-123 <kritimbastola2005@gmail.com>
2025-07-19 21:42:52 -0600

    made comment schema

---
+++ backend/models/Comment.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema({
+  videoId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "Video", 
+    required: true 
+  }, // which video/show this comment belongs to
+
+  userId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "User", 
+    required: true 
+  }, // who made the comment
+
+  text: { 
+    type: String, 
+    required: true, 
+    trim: true 
+  },
+
+  likes: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+  ], // array of users who liked it
+
+  replies: [
+    {
+      userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+      text: { type: String, required: true, trim: true },
+      createdAt: { type: Date, default: Date.now },
+    }
+  ],
+
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
+  }
+});
+
+module.exports = mongoose.model("Comment", commentSchema);
