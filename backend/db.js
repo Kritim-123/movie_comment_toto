a400cfbbdd6d9f3ca8d87cb456b4e5895ba466b0
Kritim-123 <kritimbastola2005@gmail.com>
2025-07-19 21:12:27 -0600

    changes to make migration easier later on

--- backend/db.js
+++ backend/db.js
@@ -7,9 +7,11 @@ const mongoose = require("mongoose");
  * @function connectToMongo
  * @returns {Promise} resolves when connected to the database, rejects if there is an error.
  */
+
+const MongoURL = "mongodb://127.0.0.1:27017/movie_comment";
 const connectToMongo = () => {
   mongoose
-    .connect("mongodb://127.0.0.1:27017/movie_comment")
+    .connect(MongoURL)
     .then(() => {
       console.log("Connected to database");
     })
