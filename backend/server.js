197db5a7e4f5a61e99a3c13745aa94f5aa38c3f3
Kritim-123 <kritimbastola2005@gmail.com>
2025-07-22 12:53:30 -0600

    added cors so that backend will be able to communicate with frontend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 const { connectToMongo } = require("./db");
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = 4000;
 
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-
-app.use(express.json());
 
 //Available routes
 app.use("/api/auth", require("./routes/auth"));
